a9cc5a95c0f8fb3d6c810541e415df00d09bab4d
gusttavuc1 <gusttavuc1@hotmail.com>
2024-08-22 07:58:40 -0300

    three commi

--- cars/models.py
+++ cars/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+#Isso cria uma tabela no banco de dados
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=7, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+        
+#Melhora a visualização dos nomes no BD
+    def __str__(self):
+        return self.model
     
+
     
     
